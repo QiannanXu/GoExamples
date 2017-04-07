5e982c584310fffcc5293408e4b945d17d096e72
qnxu <qiannanxu@creditease.cn>
2017-04-07 09:47:38 +0800

    3 - Variables

---
+++ 3_variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "hello"
+	fmt.Println(a)
+
+	var b, c int = 3, 5
+	fmt.Println(b, c)
+
+	d := "test"
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	var f = true
+	fmt.Println(f)
+}
